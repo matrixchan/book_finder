83acf02156d7899bfba146f89680e2a1b68c7cfc
Matrix <immatrixchan@gmail.com>
2022-03-22 15:25:38 +0800

    Navbar

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import PropTypes from 'prop-types';
+
+export default function Navbar({title,icon}) {
+  return (
+    <nav className="navbar bg-primary">
+    <h1>
+      <i className={icon} /> {title}
+    </h1>
+
+  </nav>
+  )
+}
+
+Navbar.defaultProps = {
+  title: 'Github Finder',
+  icon:'fab fa-github'
+};
+
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.string.isRequired
+}
